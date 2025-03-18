c84578359c314c531487910052a8950625c16080
Aerakim001 <106810131+Aerakim001@users.noreply.github.com>
2025-03-19 00:45:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Gangsta
-  ownerNumber: '917023951514', //919778663250
+  botOwner: 'Gangsta', // Your name
+  ownerNumber: '919778663250', //Your number 
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
